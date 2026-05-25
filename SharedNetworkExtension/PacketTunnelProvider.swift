90916ad6bd55d0a6d161350b49fc735cb833ab2d
Alan Ye <git@alany.dev>
2026-05-26 02:48:06 +0800

    fix: support packet tunnel CI toolchain

--- SharedNetworkExtension/PacketTunnelProvider.swift
+++ SharedNetworkExtension/PacketTunnelProvider.swift
@@ -6,13 +6,14 @@ final class PacketTunnelProvider: NEPacketTunnelProvider {
     private let configuration = AuroraConfiguration(endpoint: URL(string: "http://127.0.0.1:9443")!)
     private let serverClient = URLSessionAuroraServerClient()
 
-    override func startTunnel(options: [String: NSObject]?, completionHandler: @escaping @Sendable (Error?) -> Void) {
+    override func startTunnel(options: [String: NSObject]?, completionHandler: @escaping (Error?) -> Void) {
         let endpoint = configuration.endpoint
         let serverClient = serverClient
 
+        completionHandler(nil)
+
         Task {
             _ = try? await serverClient.fetchStatus(endpoint: endpoint)
-            completionHandler(nil)
         }
     }
 
